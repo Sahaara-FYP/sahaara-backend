cc04f191312c82dec14c49cf5f3ba80bef8c2a9e
Ayyan Ali <rayyanali2705@gmail.com>
2025-10-05 20:11:24 +0500

    Singleton Prisma Connection

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,13 +1,13 @@
 import { PrismaClient } from "../../generated/prisma/index.js";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-const prisma = global.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query", "error", "warn"],
+  });
 
-if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
